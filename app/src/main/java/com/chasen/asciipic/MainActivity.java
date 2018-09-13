9a05fc1fb94980bb671d5edb59e58392824d26d2
caichuangxing <zhuangsigui@bigo.sg>
2018-09-13 16:01:18 +0800

    图片转字符画，视频转字符视频实现，可以保存字符画

---
+++ app/src/main/java/com/chasen/asciipic/MainActivity.java
@@ -0,0 +1,315 @@
+package com.chasen.asciipic;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaMetadataRetriever;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Looper;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    // ---------------------- constant ---------------------- //
+
+    private static final String TAG = "AsciiActivity";
+    /**
+     * 从相册中选择图片的返回码
+     */
+    private static final int ACTION_SELECT_FORM_ALBUM = 0X001;
+    /**
+     * 相机拍摄后的返回码
+     */
+    private static final int ACTION_TAKE_PHOTO = 0X002;
+    /**
+     * 从相册选择视频的返回码
+     */
+    private static final int ACTION_PICK_VIDEO = 0X003;
+    /**
+     * 多少ms取一帧
+     */
+    private static final int GET_FRAME_PER_MS = 250;
+
+    private static final String SAVE_PATH = Environment.getExternalStorageDirectory().getPath() + "/AsciiPic/";
+
+
+    // ---------------------- field ---------------------- //
+
+    private ArrayList<Bitmap> mBitmaps = new ArrayList<>();
+    /**
+     * 显示的bitmap的下标
+     */
+    private int mIndex = 0;
+    private Handler mHandler = new Handler(Looper.getMainLooper());
+    private Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            if (mIndex < mBitmaps.size()) {
+                Bitmap bitmap = mBitmaps.get(mIndex++);
+                Log.e(TAG, "bitmap:" + bitmap);
+                if (bitmap != null) {
+                    mIm.setImageBitmap(bitmap);
+                    mHandler.postDelayed(this, 250);
+                }
+            }
+        }
+    };
+    private GetFramesTask mTask;
+    private Bitmap mCurBitmap;
+
+    // ---------------------- widget ---------------------- //
+
+    Button mSelFromAlbumBtn;
+    Button mTakePhotoBtn;
+    Button mSelVideoBtn;
+    Button mPlayBtn;
+    Button mSaveBtn;
+    ImageView mIm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ascii);
+        mSelFromAlbumBtn = findViewById(R.id.btn_select_album);
+        mTakePhotoBtn = findViewById(R.id.btn_take_photo);
+        mSelVideoBtn = findViewById(R.id.btn_select_video);
+        mPlayBtn = findViewById(R.id.btn_play);
+        mSaveBtn = findViewById(R.id.btn_save);
+        mIm = findViewById(R.id.iv);
+        mSelFromAlbumBtn.setOnClickListener(this);
+        mTakePhotoBtn.setOnClickListener(this);
+        mSelVideoBtn.setOnClickListener(this);
+        mPlayBtn.setOnClickListener(this);
+        mSaveBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_select_album:
+                jumpToAlbum();
+                break;
+            case R.id.btn_take_photo:
+                jumpToCamera();
+                break;
+            case R.id.btn_select_video:
+                jumpToVideo();
+                break;
+            case R.id.btn_play:
+                mHandler.removeCallbacks(mRunnable);
+                mIndex = 0;
+                mHandler.post(mRunnable);
+                break;
+            case R.id.btn_save:
+                saveAsPic();
+                break;
+            default:
+                break;
+        }
+    }
+
+    /**
+     * 跳转到系统相册
+     */
+    private void jumpToAlbum() {
+        Log.d(TAG, "jumpToAlbum");
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent, ACTION_SELECT_FORM_ALBUM);
+    }
+
+    /**
+     * 跳转到相机
+     */
+    private void jumpToCamera() {
+        Log.d(TAG, "jumpToCamera");
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, ACTION_TAKE_PHOTO);
+    }
+
+    /**
+     * 跳转到选择视频
+     */
+    private void jumpToVideo() {
+        Log.d(TAG, "jumpToVideo");
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("video/*");
+        startActivityForResult(intent, ACTION_PICK_VIDEO);
+    }
+
+    /**
+     * 保存图片
+     */
+    private void saveAsPic() {
+        if (mCurBitmap == null) {
+            Toast.makeText(this, "图片为空，保存失败", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        // 创建目录
+        File dir = new File(SAVE_PATH);
+        if (!dir.exists()) {
+            dir.mkdir();
+        }
+        String path = SAVE_PATH + System.currentTimeMillis() + ".jpg";
+        File file = new File(path);
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(file);
+            mCurBitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            Toast.makeText(this, "保存成功，保存路径："+path, Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, "保存失败", Toast.LENGTH_SHORT).show();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            // 从相册返回来
+            if (requestCode == ACTION_SELECT_FORM_ALBUM) {
+                Uri uri = data.getData();
+                String[] filePathColumn = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getContentResolver().query(uri, filePathColumn, null, null, null);
+                cursor.moveToFirst();
+                String path = cursor.getString(cursor.getColumnIndex(filePathColumn[0]));
+                cursor.close();
+                Log.d(TAG, "path:" + path);
+                Bitmap bitmap = BitmapFactory.decodeFile(path);
+                if (bitmap != null) {
+                    mCurBitmap = PicToAsciiUitl.createAsciiPic(bitmap, MainActivity.this);
+                    mIm.setImageBitmap(mCurBitmap);
+                }
+            }
+            // 从拍照返回来
+            else if (requestCode == ACTION_TAKE_PHOTO) {
+                Bitmap bitmap = data.getParcelableExtra("data");
+                if (bitmap != null) {
+                    mCurBitmap = PicToAsciiUitl.createAsciiPic(bitmap, MainActivity.this);
+                    mIm.setImageBitmap(mCurBitmap);
+                } else {
+                    Log.e(TAG, "bitmap is null");
+                }
+            }
+            // 选择视频
+            else if (requestCode == ACTION_PICK_VIDEO) {
+                mBitmaps.clear();
+                Log.d(TAG, "pick video");
+                Uri uri = data.getData();
+                mTask = new GetFramesTask(this);
+                mTask.execute(uri);
+            }
+        } else {
+            Log.e(TAG, "select pic fail");
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mTask.cancel(true);
+        mBitmaps.clear();
+        mBitmaps = null;
+    }
+
+    /**
+     * 将视频转化为一帧一帧的Bitmap，然后用Handler播放出来
+     */
+    private class GetFramesTask extends AsyncTask<Uri, Integer, List<Bitmap>> {
+
+        private Context context;
+        private AlertDialog dialog;
+        private TextView mProgressTv;
+        // 一共取了多少帧
+        private int size;
+
+        public GetFramesTask(Context context) {
+            this.context = context;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            View view = LayoutInflater.from(context).inflate(R.layout.dialog_loading, null);
+            mProgressTv = view.findViewById(R.id.txt_progress);
+            dialog = new AlertDialog.Builder(context)
+                    .setView(view)
+                    .setCancelable(false)
+                    .create();
+            dialog.show();
+        }
+
+        @Override
+        protected List<Bitmap> doInBackground(Uri... uris) {
+            MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+            mmr.setDataSource(context, uris[0]);
+            size = Integer.parseInt(mmr.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION)) / GET_FRAME_PER_MS;
+            int index = 0;
+            Log.e(TAG, "size:" + size);
+            long startTime = System.currentTimeMillis();
+            while (index < size + 1) {
+                Bitmap bitmap = mmr.getFrameAtTime(index * 1000 * GET_FRAME_PER_MS, MediaMetadataRetriever.OPTION_CLOSEST);
+                if (bitmap != null) {
+                    mBitmaps.add(PicToAsciiUitl.createAsciiPic(bitmap, MainActivity.this));
+                }
+                Log.d(TAG, "index:" + index);
+                publishProgress(index);
+                index++;
+            }
+            Log.d(TAG, "spent time:" + (System.currentTimeMillis() - startTime));
+            return mBitmaps;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            mProgressTv.setText("(" + values[0] + "/" + size + ")");
+        }
+
+        @Override
+        protected void onPostExecute(List<Bitmap> bitmaps) {
+            super.onPostExecute(bitmaps);
+            dialog.dismiss();
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            mBitmaps.clear();
+            mBitmaps = null;
+            context = null;
+        }
+    }
+}
---
+++ app/src/main/java/com/chasen/asciipic/PicToAsciiUitl.java
@@ -0,0 +1,109 @@
+package com.chasen.asciipic;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.WindowManager;
+
+/**
+ * @Author Chasen
+ * @Data 2018/9/12
+ *
+ * 将图片转换成Ascii
+ * 将Ascii转成图片
+ *
+ */
+public class PicToAsciiUitl {
+
+    public static final String TAG = "PicToAsciiUtil";
+
+    /**
+     * 创建一个字符Bimap
+     * @param image Bitmap，原始图片
+     * @param context Context
+     * @return 转成Ascii图片再转成Bitmap并返回
+     */
+    public static Bitmap createAsciiPic(Bitmap image, Context context) {
+        // 字符串由复杂到简单
+        final String base = "#8XOHLTI)i=+;:,.";
+        StringBuilder text = new StringBuilder();
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        int width0 = image.getWidth();
+        int height0 = image.getHeight();
+        int width1, height1;
+        int scale = 7;
+        if (width0 <= width / scale) {
+            width1 = width0;
+            height1 = height0;
+        } else {
+            width1 = width / scale;
+            height1 = width1 * height0 / width0;
+        }
+        //读取图片
+        image = scale(image, width1, height1);
+        //输出到指定文件中
+        for (int y = 0; y < image.getHeight(); y += 2) {
+            for (int x = 0; x < image.getWidth(); x++) {
+                final int pixel = image.getPixel(x, y);
+                final int r = (pixel & 0xff0000) >> 16, g = (pixel & 0xff00) >> 8, b = pixel & 0xff;
+                final float gray = 0.299f * r + 0.578f * g + 0.114f * b;
+                final int index = Math.round(gray * (base.length() + 1) / 255);
+                String s = index >= base.length() ? " " : String.valueOf(base.charAt(index));
+                text.append(s);
+            }
+            text.append("\n");
+        }
+        return textAsBitmap(text, context);
+    }
+
+    /**
+     * 缩放
+     * @param bitmap 原始Bitmap
+     * @param newWidth 新的宽
+     * @param newHeight 新的高
+     * @return 缩小后的Bitmap
+     */
+    public static Bitmap scale(Bitmap bitmap, int newWidth, int newHeight) {
+        Bitmap ret = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
+        return ret;
+    }
+
+    /**
+     * 将Ascii字符转成Bitmap
+     * @param text Ascii字符
+     * @param context Context
+     * @return 生成的Ascii字符Bitmap
+     */
+    public static Bitmap textAsBitmap(StringBuilder text, Context context) {
+        TextPaint textPaint = new TextPaint();
+        textPaint.setColor(Color.BLACK);
+        textPaint.setAntiAlias(true);
+        textPaint.setTypeface(Typeface.MONOSPACE);
+        textPaint.setTextSize(12);
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        DisplayMetrics dm = new DisplayMetrics();
+        wm.getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels;
+        StaticLayout layout = new StaticLayout(text, textPaint, width,
+                Layout.Alignment.ALIGN_CENTER, 1f, 0.0f, true);
+        Bitmap bitmap = Bitmap.createBitmap(layout.getWidth() + 20,
+                layout.getHeight() + 20, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        canvas.translate(10, 10);
+        canvas.drawColor(Color.WHITE);
+        layout.draw(canvas);
+        Log.d("textAsBitmap", String.format("1:%d %d", layout.getWidth(), layout.getHeight()));
+        return bitmap;
+    }
+
+}
